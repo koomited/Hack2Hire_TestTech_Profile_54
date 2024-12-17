21e35aa30419089f1b9257b1e898695f619628ba
koomited <koomi@aims.ac.za>
2024-12-17 22:19:28 +0200

    [UPDATE] streamlit app

--- model.py
+++ model.py
@@ -2,8 +2,42 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 st.markdown(
     "<h2 style='text-align: center;'>Model Results</h2>", 
     unsafe_allow_html=True
 )
+
+features =  pd.read_csv("features.csv")
+features = features.columns.to_list()
+
+importances =  pd.read_csv("importances.csv")
+importances = [round(float(num),4) for num in importances.columns.to_list()]
+
+st.markdown(
+    "<h2 style='text-align: right;'>Model Accuracy: 0.655</h2>", 
+    unsafe_allow_html=True
+)
+
+
+st.subheader("Features importances")
+
+
+fig, ax = plt.subplots(figsize=(7, 6), dpi=500)
+
+# Sort the indices by importances
+indices = np.argsort(importances)
+
+# Plot horizontal bar chart
+ax.barh(range(len(indices)), np.array(importances)[indices], color='teal', align='center')
+ax.set_yticks(range(len(indices)))
+ax.set_yticklabels([features[i] for i in indices])
+ax.set_xlabel('Relative Importance', fontsize=10)
+
+# Display using st.pyplot
+st.pyplot(fig)
+
+
+
